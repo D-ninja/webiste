5e81fb470af8d140e13c0b06e26bf7a8f9334680
Pranjal Tripathi <72517705+D-ninja@users.noreply.github.com>
2024-09-07 15:41:31 +0530

    Update install_dependencies.sh

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -2,6 +2,9 @@
 # Install dependencies if needed
 echo "Installing dependencies..."
 apt-get update
+sudo apt -y install unzip curl
 apt-get install -y nginx
 echo "Dependencies installed."
-
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
